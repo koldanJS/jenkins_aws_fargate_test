bad4a4417db7b9e944071005b118603439bf88de
KoldanJS <daniil.kolodkin@satelliteinnovations.io>
2023-01-09 20:47:03 +0300

    test commit2

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,17 @@
-def CLOUD = 'digifi-staging'
-def GITHUB_CREDENTIAL_ID = 'github-digifi'
+@Library('test-lib-shared') _
+
+def CLOUD = 'test-staging'
+def GITHUB_CREDENTIAL_ID = 'github-test-creds'
 def NAMESPACE = 'apps'
 def MIGRATION_CHECK = false
+
+properties([
+    parameters([
+        param.getGithubRepoReleases(
+            github_repo:'https://api.github.com/repos/koldanJS/jenkins_aws_fargate_test/releases',
+            github_credentials_id: "${GITHUB_CREDENTIAL_ID}")
+    ])
+])
 
 pipeline {
     agent any
@@ -17,17 +27,17 @@ pipeline {
         disableConcurrentBuilds()
     }
     environment {
-      AWS_REGION = 'us-east-1'
-      AWS_CREDENTIALS_ID = 'digifi-staging-deployment-user'
-      AWS_SECRETS_MANAGER_ID = 'arn:aws:secretsmanager:us-east-1:266311286062:secret:digifi/staging/application-dVcQU09D-TmX6IX'
+      AWS_REGION = 'eu-central-1'
+      AWS_CREDENTIALS_ID = 'test-staging-deployment-user'
+      AWS_SECRETS_MANAGER_ID = 'arn:aws:secretsmanager:eu-central-1:266311286062:secret:test/staging/application-dVcQU09D-TmX6IX'
       DEFAULT_INGRESS_HOST = 'staging.digifi.cc'
-      ECR_REGISTRY = '439232230176.dkr.ecr.us-east-1.amazonaws.com'
+      ECR_REGISTRY = '439232230176.dkr.ecr.eu-central.amazonaws.com'
       ENVIRONMENT_NAME = 'staging'
       GIT_REPO_TOKEN = credentials("${GITHUB_CREDENTIAL_ID}")
       GITHUB_CREDENTIAL_ID = "${GITHUB_CREDENTIAL_ID}"
       NAMESPACE = "${NAMESPACE}"
       HELM_EXPERIMENTAL_OCI = 1
-      HELM_VALUES_S3_BUCKET = 'digifi-helm-values-eyjhbgcioijiuzi1niisinr5cci6ikpxvcj9' 
+      HELM_VALUES_S3_BUCKET = 'test-helm-values-eyjhbgcioijiuzi1niisinr5cci6ikpxvcj9' 
       SECRET_NAME_OR_ARN = 'secrets/migrations/test2'
       SECRETS_MIGRATIONS_PATH = 'secrets-migrator/secrets'
     }
---
+++ test-lib-shared/vars/param.groovy
@@ -0,0 +1,62 @@
+
+def getGithubRepoReleases(Map config = [:]) {
+  if ( config['github_repo'] == null ) {
+    error(['"github_repo" argument is mandatory'])
+  }
+  if ( config['github_credentials_id'] == null ) {
+    error(['"github_credentials_id" argument is mandatory'])
+  }
+
+  github_repo = config['github_repo']
+  github_credentials_id =  config['github_credentials_id']
+
+  return [$class: 'CascadeChoiceParameter',
+                choiceType: 'PT_SINGLE_SELECT',
+                description: 'The names',
+                filterLength: 1,
+                filterable: false,
+                name: 'RELEASE_VERSION',
+                randomName: 'choice-parameter-5631314439613978',
+                script: [
+                    $class: 'GroovyScript',
+                    fallbackScript: [
+                      sandbox: true,
+                      script : 'return ["error"]'
+                    ],
+                    script: [
+                        classpath: [],
+                        sandbox: true,
+                        script: """import groovy.json.JsonSlurper
+                                   import jenkins.model.*
+                                   try {
+                                        def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+                                        com.cloudbees.plugins.credentials.common.StandardUsernameCredentials, Jenkins.instance, null, null
+                                        ).find {
+                                            it.id == '$github_credentials_id'
+                                        }
+                                        def http = new URL('$github_repo').openConnection() as HttpURLConnection
+                                        http.setRequestMethod('GET')
+                                        http.setDoOutput(true)
+                                        http.setRequestProperty('Accept', 'application/json')
+                                        http.setRequestProperty('Authorization', "token \${creds.password}")
+                                        http.connect()
+                                        def response = [:]
+                                        if (http.responseCode == 200) {
+                                            response = new JsonSlurper().parseText(http.inputStream.getText('UTF-8'))
+                                            def resArr = []
+                                            response .each { it->
+                                                resArr.push(it.tag_name)
+                                            }
+                                            return resArr
+                                        } else {
+                                            response = new JsonSlurper().parseText(http.errorStream.getText('UTF-8'))
+                                            return [response]
+                                        }
+                                   } catch (Exception e) {
+                                        return ["error"]
+                                   }
+                                """
+                    ]
+                ]
+            ]
+}
