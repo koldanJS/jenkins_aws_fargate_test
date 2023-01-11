29553e9f727d889cf9851bd52069be7c83353766
KoldanJS <daniil.kolodkin@satelliteinnovations.io>
2023-01-11 13:27:56 +0300

    test commit2

--- test-lib-shared/vars/param.groovy
+++ test-lib-shared/vars/param.groovy
@@ -10,52 +10,52 @@ def getGithubRepoReleases(Map config = [:]) {
   github_credentials_id =  config['github_credentials_id']
 
   return [$class: 'CascadeChoiceParameter',
-                choiceType: 'PT_SINGLE_SELECT',
-                description: 'The names',
-                filterLength: 1,
-                filterable: false,
-                name: 'RELEASE_VERSION',
-                randomName: 'choice-parameter-5631314439613978',
-                script: [
-                    $class: 'GroovyScript',
-                    fallbackScript: [
-                      sandbox: true,
-                      script : 'return ["error"]'
-                    ],
-                    script: [
-                        classpath: [],
-                        sandbox: true,
-                        script: """
-                          import groovy.json.JsonSlurper
-                          import jenkins.model.*
-                          try {
-                              def creds = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getCredentials()
-                              def cred = creds.find {
-                                  it.id == '$github_credentials_id'
-                              }
-                              def http = new URL('$github_repo').openConnection() as HttpURLConnection
-                              http.setRequestMethod('GET')
-                              http.setDoOutput(true)
-                              http.setRequestProperty('Accept', 'application/json')
-                              http.setRequestProperty('Authorization', "token \${cred.password}")
-                              http.connect()
-                              def response = [:]
-                              if (http.responseCode == 200) {
-                                  response = new JsonSlurper().parseText(http.inputStream.getText('UTF-8'))
-                                  def resArr = []
-                                  response .each { it->
-                                      resArr.push(it.tag_name)
-                                  }
-                                  return resArr
-                              } else {
-                                  response = new JsonSlurper().parseText(http.errorStream.getText('UTF-8'))
-                                  return [response]
-                              }
-                          } catch (Exception e) {
-                              return ["error"]
-                          }
-                        """
-                    ]
-                ]
-            ]
+          choiceType: 'PT_SINGLE_SELECT',
+          description: 'Releases',
+          filterLength: 1,
+          filterable: false,
+          name: 'RELEASE_VERSION',
+          randomName: 'choice-parameter-5631314439613978',
+          script: [
+              $class: 'GroovyScript',
+              fallbackScript: [
+                sandbox: true,
+                script : 'return ["error"]'
+              ],
+              script: [
+                  classpath: [],
+                  sandbox: true,
+                  script: """
+                    import groovy.json.JsonSlurper
+                    import jenkins.model.*
+                    try {
+                        def creds = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getCredentials()
+                        def cred = creds.find {
+                            it.id == '$github_credentials_id'
+                        }
+                        def http = new URL('$github_repo').openConnection() as HttpURLConnection
+                        http.setRequestMethod('GET')
+                        http.setDoOutput(true)
+                        http.setRequestProperty('Accept', 'application/json')
+                        http.setRequestProperty('Authorization', "token \${cred.password}")
+                        http.connect()
+                        def response = [:]
+                        if (http.responseCode == 200) {
+                            response = new JsonSlurper().parseText(http.inputStream.getText('UTF-8'))
+                            def resArr = []
+                            response .each { it->
+                                resArr.push(it.tag_name)
+                            }
+                            return resArr
+                        } else {
+                            response = new JsonSlurper().parseText(http.errorStream.getText('UTF-8'))
+                            return [response]
+                        }
+                    } catch (Exception e) {
+                        return ["error"]
+                    }
+                  """
+              ]
+          ]
+  ]
 }
